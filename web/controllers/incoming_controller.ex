13624072b2d48f57d414cb04f9856735de2665d1
Rik Lomas <lomas.rik@gmail.com>
2016-06-16 21:57:03 +0100

    incoming inspect

--- web/controllers/incoming_controller.ex
+++ web/controllers/incoming_controller.ex
@@ -1,5 +1,12 @@
 defmodule Bankbot.IncomingController do
   use Bankbot.Web, :controller
+  
+  def index(conn, params) do
+    Logger.info "#{inspect params}"
+    IO.puts "#{inspect params}"
+    
+    text(conn, "Done") 
+  end
 
   def create(conn, params) do
     Logger.info "#{inspect params}"
